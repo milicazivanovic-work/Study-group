225b08887794ad251afa27983f5ba11f1403a5c2
Milica Zivanovic <milica.zivanovic@work.co>
2022-10-10 14:46:36 +0200

    Hello name finally works

--- hello.js
+++ hello.js
@@ -1,31 +1,6 @@
-// Gets user input
-/*var prompt = require('syncprompt');
-var name = prompt("What is your name? ");
-
-// Uses  to print out information
-function printHelloName(){
-  for (var i = 0; i < 5; i++) {
-      console.log("Hello " + name);
-    }
-  }  
-printHelloName();
-*/
-
-// With paramater name
-/*function printHelloName(name){
-  name = "Milica";
-  for (var i = 0; i < 5; i++) {
-      console.log("Hello " + name);
-    }
-  }  
-printHelloName();
-*/
-
-//Command Line Argument
 function printHelloName(name) {
-  if (process.argv[2] != '' || process.argv[2] != isNaN) {
-    name = process.argv[2];
-    console.log('Hello', name);
+  if (process.argv[2] != undefined) {
+    console.log('Hello', process.argv[2]);
   } else {
     console.log('Hello', name);
   }
