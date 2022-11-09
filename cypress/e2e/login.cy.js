6975a43e72741738d14f55a4acee5c44028d8aaf
milicazivanovic-work <111513250+milicazivanovic-work@users.noreply.github.com>
2022-11-09 11:29:06 +0100

    Challenge 4 Swag Labs test (#4)
    * Challenge 4 Swag Labs test
    
    * Grouping test in describe, test case titles change

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,26 @@
+describe('Swag Labs login test', () => {
+    it('Opens the Swag Labs website and checks for the matching page title', () => {
+      cy.visit('https://www.saucedemo.com/');
+      cy.title().should('eq','Swag Labs');
+    })
+  
+    it('Enters invalid credentials and asserts the error message', () => {
+      cy.visit('https://www.saucedemo.com/')
+      cy.get('[data-test="username"]').click()
+      cy.get('[data-test="username"]').type('milica.zivanovic@work.co')
+      cy.get('[data-test="password"]').click()
+      cy.get('[data-test="password"]').type('secret_sauce')
+      cy.get('[data-test="login-button"]').click()
+      cy.get('[data-test="error"]').should('have.text','Epic sadface: Username and password do not match any user in this service');
+    })
+
+    it('Enters valid credentials and checks the URL', () => {
+      cy.visit('https://www.saucedemo.com/')
+      cy.get('[data-test="username"]').click()
+      cy.get('[data-test="username"]').type('standard_user')
+      cy.get('[data-test="password"]').click()
+      cy.get('[data-test="password"]').type('secret_sauce')
+      cy.get('[data-test="login-button"]').click()
+      cy.url().should('contains', 'inventory.html');
+    })
+  })
