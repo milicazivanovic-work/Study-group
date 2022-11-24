24264b030fc78703b1341446418d29f255eeb142
Milica Zivanovic <milica.zivanovic@work.co>
2022-11-24 16:01:55 +0100

    Fifth challenge - adding login and commands.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,4 @@
-describe('Swag Labs login test', () => {
+describe('Swag Labs login and logout test', () => {
     it('Opens the Swag Labs website and checks for the matching page title', () => {
       cy.visit('https://www.saucedemo.com/');
       cy.title().should('eq','Swag Labs');
@@ -22,5 +22,16 @@ describe('Swag Labs login test', () => {
       cy.get('[data-test="password"]').type('secret_sauce')
       cy.get('[data-test="login-button"]').click()
       cy.url().should('contains', 'inventory.html');
+    })
+
+    it('Enters valid credentials and logs out', () => {
+      cy.visit('https://www.saucedemo.com/')
+      cy.get('[data-test="username"]').click()
+      cy.get('[data-test="username"]').type('standard_user')
+      cy.get('[data-test="password"]').click()
+      cy.get('[data-test="password"]').type('secret_sauce')
+      cy.get('[data-test="login-button"]').click()
+      cy.get('#react-burger-menu-btn').click()
+      cy.get('#logout_sidebar_link').click();
     })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,16 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type(username)
+    cy.get('[data-test="password"]').type(password)
+    cy.get('[data-test="login-button"]').click()
+    cy.url().should('contains', 'inventory.html')
+    })
+
+//Cypress.Commands.overwrite('visit', (orig, url, options) => {})
+
 //
 //
 // -- This is a child command --
